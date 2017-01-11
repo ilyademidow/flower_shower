1ece22bbb146b3102dba183109496c35f974acce
Ilya Demidov <ilyademidow@mail.ru>
2017-01-11 18:24:55 +0000

    Simplify script by using DO

--- flower_shower.py
+++ flower_shower.py
@@ -1,46 +1,34 @@
-# Reading an analogue sensor with
-# a single GPIO pin
+# The simplest script for detection soil moistures
+# Tuning YL-38 and connect DO to Raspberry Pi Model 3
+# Output is True or False for current moisture
 
-# Author : Matt Hawkins
+# Author : Ilya Demidov
 # Distribution : Raspbian
-# Python : 2.7
+# Python : 3
 # GPIO   : RPi.GPIO v3.1.0a
+import time, configparser
+from gpiozero import MotionSensor, OutputDevice
 
-import RPi.GPIO as GPIO, time
-from gpiozero import OutputDevice
+# Read Mode from config
+config = configparser.ConfigParser()
+config.read("mode.py")
+debugMode = config.getboolean("Mode","debugMode")
 
-# Tell the GPIO library to use
-# Broadcom GPIO references
-GPIO.setmode(GPIO.BCM)
-outD = OutputDevice(17)
-
-# Define function to measure charge time
-print("LOW " + str(GPIO.LOW))
-print("OUT " + str(GPIO.OUT))
-def RCtime (PiPin):
-  measurement = 0
-  # Discharge capacitor
-  GPIO.setup(PiPin, GPIO.OUT)
-  GPIO.output(PiPin, GPIO.LOW)
-  time.sleep(0.01)
-
-  GPIO.setup(PiPin, GPIO.IN)
-  # Count loops until voltage across
-  # capacitor reads high on GPIO
-  for i in range(0,1000):
-    if (GPIO.input(PiPin) == GPIO.LOW):
-        measurement += 1
-
-  return measurement
+# MotionSensor class is useful for this target
+ms = MotionSensor(18)
+# WaterPomp
+waterPomp = OutputDevice(17)
 
 # Main program loop
 while True:
-  moisture = int(float(RCtime(9)))
-  print(moisture) # Measure timing using GPIO4
-  if(moisture <= 500):
-      print("true (<500)")
-      outD.on()
+  if ms.motion_detected:
+      if debugMode:
+        print("on")
+      else:
+        waterPomp.on()
       time.sleep(3)
   else:
-      print("false (<500)")
-      outD.off()
+      if debugMode:
+        print("OFF")
+      else:
+        waterPomp.off()
